bcd5ca2681ee0a6ca929a9893a589034e40da2a8
programmingwbn <106359420+programmingwbn@users.noreply.github.com>
2023-08-15 19:12:00 -0700

    Update Problem#2.1.cpp

--- Problem#2.1.cpp
+++ Problem#2.1.cpp
@@ -1,27 +1,23 @@
 #include <iostream>
 using namespace std;
 int main() {
-int first_value, second_value, third_value, answer, x;
-first_value = 1;
-second_value = 2;
-x = 0;
-for (x < 4000000; x;){
-   
-    third_value = first_value + second_value;
-   if (x > 4000000) {
-       break;
-   }
-   
-    else if (third_value % 2 == 0) {
-        answer += third_value;
-        third_value = x;
+int t1,t2,new_term, n,x;
+t1 = 1;
+t2 = 2;
+new_term = t1+t2;
+n = 4000000;
+while (new_term <= n) {
+    if (new_term % 2 == 0){
+        x+=new_term;
         
     }
-    else if (third_value % 2 != 0) {
-        answer = answer;
-        third_value = x;
-    }
 
+    t1 = t2;
+    t2 = new_term;
+    new_term = t1+t2;
+    
 }
-cout << answer;
+cout << x+2;
+
+
 return 0; }
