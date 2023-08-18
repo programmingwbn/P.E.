c4db85d352a8599cd02c5b8d3d1b266c571b46d0
programmingwbn <106359420+programmingwbn@users.noreply.github.com>
2023-08-17 17:49:40 -0700

    Update problem#3.1.cpp

--- problem#3.1.cpp
+++ problem#3.1.cpp
@@ -4,19 +4,34 @@ using namespace std;
 
 int main()
 {
-    int count,num ;
+    // int count,num, ;
     long int x = 600851475143;
-    while (x = x) {
-        x--;
-        if (x % 2 != 0||x % 3 != 0||x % 5 != 0||x % 7 != 0||x % 11 != 0||x % 13 != 17||x % 19 != 0||x % 23 != 0 ||x % 29 != 0) {
-            count = x;
-            cout << count;
-            break;
+    
+    
+    // while (x = x) {
+    //     x--;
+    //     if (x % 2 != 0||x % 3 != 0||x % 5 != 0||x % 7 != 0||x % 11 != 0||x % 13 != 17||x % 19 != 0||x % 23 != 0 ||x % 29 != 0) {
+    //         count = x;
+    //         cout << count;
+    //         break;
         
+    // }
+    // }
+    // num=x/count;
+    // cout << endl << x << count << num;
+    
+    int prime;
+    prime = 100000;
+    // int list[] = {};
+    while (prime > 0) {
+        prime--;
+        for (int i = prime; i >= 0; --i)
+             if (prime % i == 2) {
+                cout << i;
+             }
+             else if (prime % i == 1) {
+                 cout << i;
+             }
+        // cout << i << "\n";
     }
-    }
-    num=x/count;
-    cout << endl << x << count << num;
-
-    return 0;
-}
+        
