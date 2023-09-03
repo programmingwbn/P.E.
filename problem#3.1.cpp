20cda1c2d0ed7d6276bd7df218e5d53fe882ab71
programmingwbn <106359420+programmingwbn@users.noreply.github.com>
2023-09-03 12:11:56 -0700

    Update problem#3.1.cpp

--- problem#3.1.cpp
+++ problem#3.1.cpp
@@ -3,35 +3,51 @@
 using namespace std;
 
 int main()
+// {
+//     // int count,num, ;
+//     long int x = 600851475143;
+    
+    
+//     // while (x = x) {
+//     //     x--;
+//     //     if (x % 2 != 0||x % 3 != 0||x % 5 != 0||x % 7 != 0||x % 11 != 0||x % 13 != 17||x % 19 != 0||x % 23 != 0 ||x % 29 != 0) {
+//     //         count = x;
+//     //         cout << count;
+//     //         break;
+        
+//     // }
+//     // }
+//     // num=x/count;
+//     // cout << endl << x << count << num;
+    
+//     int prime;
+//     prime = 100000;
+//     // int list[] = {};
+//     while (prime > 0) {
+//         prime--;
+//         for (int i = prime; i >= 0; --i)
+//              if (prime % i == 2) {
+//                 cout << i;
+//              }
+//              else if (prime % i == 1) {
+//                  cout << i;
+//              }
+//         // cout << i << "\n";
+//     }
 {
-    // int count,num, ;
-    long int x = 600851475143;
+int n, z;
+n = 2;
+long int x=600851475143;
+
+while(x >0) {
     
-    
-    // while (x = x) {
-    //     x--;
-    //     if (x % 2 != 0||x % 3 != 0||x % 5 != 0||x % 7 != 0||x % 11 != 0||x % 13 != 17||x % 19 != 0||x % 23 != 0 ||x % 29 != 0) {
-    //         count = x;
-    //         cout << count;
-    //         break;
-        
-    // }
-    // }
-    // num=x/count;
-    // cout << endl << x << count << num;
-    
-    int prime;
-    prime = 100000;
-    // int list[] = {};
-    while (prime > 0) {
-        prime--;
-        for (int i = prime; i >= 0; --i)
-             if (prime % i == 2) {
-                cout << i;
-             }
-             else if (prime % i == 1) {
-                 cout << i;
-             }
-        // cout << i << "\n";
+
+    for (x >0; x--; n*n < 600851475143) {
+            n++;
+     if (x % n ==0) {
+        std:cout << x << "\n" << n << "\n" << "\n";
     }
-        
+
+}
+}
+}
